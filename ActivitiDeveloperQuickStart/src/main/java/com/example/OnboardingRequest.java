fea9be4427c4c34181e0fb79fc4322ac872f75bc
DELL <DELL@192.168.191.3>
2017-05-04 15:11:50 +0800

    按照快速入门程序写到80%，剩下的由于网络问题暂停

--- ActivitiDeveloperQuickStart/src/main/java/com/example/OnboardingRequest.java
+++ ActivitiDeveloperQuickStart/src/main/java/com/example/OnboardingRequest.java
@@ -2,10 +2,16 @@ package com.example;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 
 /**
- * 工作流示例：一个登机请求
+ * 工作流示例：一个入职请求
+ * 过程：1利用jdbc连接数据库创建一个默认的工作流引擎，打印引擎和名字
+ * 			加载流程描述文件，创建流程实体，打印流程的名字和ID
+ * 由于该网络问题，以下代码完成不可。只能待有机会再补。该示例程序暂停
  * @author DELL
  *
  */
@@ -24,6 +30,16 @@ public class OnboardingRequest {
 	    String ver = ProcessEngine.VERSION;
 	    //打印Process Engine名字和Activiti版本。
 	    System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+	    //利用该xml创建一个工作流进程。打印该任务
+	    RepositoryService repositoryService = processEngine.getRepositoryService();
+	    Deployment deployment = repositoryService.createDeployment()
+	        .addClasspathResource("onboarding.bpmn20.xml").deploy();
+	    ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+	        .deploymentId(deployment.getId()).singleResult();
+	    System.out.println(
+	        "Found process definition [" 
+	            + processDefinition.getName() + "] with id [" 
+	            + processDefinition.getId() + "]");
 	  }
 	} 
 
